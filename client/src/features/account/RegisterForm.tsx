2492193f9657359fc371cdebd8cb96073a92c94f
juviZ138 <quachlydientam@gmail.com>
2025-12-03 13:57:33 +0700

    feat: Email confirmation

--- client/src/features/account/RegisterForm.tsx
+++ client/src/features/account/RegisterForm.tsx
@@ -9,12 +9,16 @@ import {
   registerSchema,
   type RegisterSchema,
 } from "../../lib/schemas/registerSchema";
+import { useState } from "react";
+import RegisterSuccess from "./RegisterSuccess";
 
 export default function RegisterForm() {
   const { registerUser } = useAccount();
+  const [registerSuccess, setRegisterSuccess] = useState(false);
   const {
     control,
     handleSubmit,
+    watch,
     setError,
     formState: { isValid, isSubmitting },
   } = useForm<RegisterSchema>({
@@ -22,8 +26,11 @@ export default function RegisterForm() {
     resolver: zodResolver(registerSchema),
   });
 
+  const email = watch("email");
+
   const onSubmit = async (data: RegisterSchema) => {
     await registerUser.mutateAsync(data, {
+      onSuccess: () => setRegisterSuccess(true),
       onError: (error) => {
         if (Array.isArray(error)) {
           error.forEach((err) => {
@@ -37,52 +44,62 @@ export default function RegisterForm() {
   };
 
   return (
-    <Paper
-      component={"form"}
-      onSubmit={handleSubmit(onSubmit)}
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        p: 3,
-        gap: 3,
-        maxWidth: "md",
-        mx: "auto",
-        borderRadius: 3,
-      }}
-    >
-      <Box
-        display="flex"
-        alignItems={"center"}
-        justifyContent={"center"}
-        gap={3}
-        color={"secondary.main"}
-      >
-        <LockOpen fontSize="large" />
-        <Typography variant="h4">Register</Typography>
-      </Box>
+    <>
+      {registerSuccess ? (
+        <RegisterSuccess email={email} />
+      ) : (
+        <Paper
+          component={"form"}
+          onSubmit={handleSubmit(onSubmit)}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            p: 3,
+            gap: 3,
+            maxWidth: "md",
+            mx: "auto",
+            borderRadius: 3,
+          }}
+        >
+          <Box
+            display="flex"
+            alignItems={"center"}
+            justifyContent={"center"}
+            gap={3}
+            color={"secondary.main"}
+          >
+            <LockOpen fontSize="large" />
+            <Typography variant="h4">Register</Typography>
+          </Box>
 
-      <TextInput label="Email" name="email" control={control} />
-      <TextInput label="Display name" name="displayName" control={control} />
-      <TextInput
-        label="Password"
-        type="password"
-        name="password"
-        control={control}
-      />
-      <Button
-        type="submit"
-        disabled={!isValid || isSubmitting}
-        variant="contained"
-        size="large"
-      >
-        Register
-      </Button>
-      <Typography sx={{ textAlign: "center" }}>
-        Already have an account?{" "}
-        <Typography component={Link} to={"/login"} color="primary">
-          Sign in
-        </Typography>
-      </Typography>
-    </Paper>
+          <TextInput label="Email" name="email" control={control} />
+          <TextInput
+            label="Display name"
+            name="displayName"
+            control={control}
+          />
+          <TextInput
+            label="Password"
+            type="password"
+            name="password"
+            control={control}
+          />
+          <Button
+            type="submit"
+            disabled={!isValid || isSubmitting}
+            variant="contained"
+            size="large"
+          >
+            Register
+          </Button>
+          <Typography sx={{ textAlign: "center" }}>
+            Already have an account?{" "}
+            <Typography component={Link} to={"/login"} color="primary">
+              Sign in
+            </Typography>
+          </Typography>
+        </Paper>
+      )}
+    </>
   );
 }
